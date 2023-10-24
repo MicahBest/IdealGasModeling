1eb06bbd4554b2cd26741d067f2b507164d46805
Micah <micahbest21@gmail.com>
2023-10-24 11:20:56 -0500

    Updated text-to-csv script

--- IGmodel/scripts/txt-to-csv.py
+++ IGmodel/scripts/txt-to-csv.py
@@ -1,10 +1,10 @@
 import os
 import pandas as pd
 
-def txt_to_csv(file):
+def txt_to_csv(file, path="./", output_path="./"):
     """Converts txt file to csv"""
     data = []
-    with open(file+".txt", 'r') as rf:
+    with open(path+file+".txt", 'r') as rf:
         for nrow, row in enumerate(rf):
             if nrow == 0:
                 header = row.split()
@@ -14,19 +14,12 @@ def txt_to_csv(file):
                 data.append(row_dict)
 
     df = pd.DataFrame(data)
-    df.to_csv(file+".csv", index=False)
+    df.to_csv(output_path+file+".csv", index=False)
 
 if __name__ == "__main__":
-    path = "IGmodel/property-tables/"
-    files = os.listdir(path)
+    input_path = "IGmodel/property-tables/_txt/"
+    output_path = "IGmodel/property-tables/"
+    files = os.listdir(input_path)
     for file in files:
         if file.endswith(".txt"):
-            file_path = os.path.join(path, file)
-            txt_to_csv(file_path.replace(".txt", ''))
-
-            # move txt to different directory
-            new_path = path+"_txt/"
-            txt_path = os.path.join(new_path, file)
-            if not os.path.exists(new_path):
-                os.mkdir(new_path)
-            os.rename(file_path, txt_path)
+            txt_to_csv(file.replace(".txt", ""), input_path, output_path)
