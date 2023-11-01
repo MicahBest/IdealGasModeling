03ebefd879b95f2fdc939ac67456c080d312cc2f
MicahBest <micahbest21@gmail.com>
2023-11-01 10:36:25 -0500

    Added mixture class

--- IGmodel/materials.py
+++ IGmodel/materials.py
@@ -7,55 +7,18 @@ PROPERTY_TABLE_PATH = "IGmodel/property-tables/"
 
 @add_metaclass(abc.ABCMeta)
 class Material:
-    def __init__(self, M, R) -> None:
-        self._M = M # kg/kmol
-        self._R = R # kJ/kg-K
+    def __init__(self, M) -> None:
+        self.M = M # kg/kmol
 
     @staticmethod
     def _interpolate(x, x_known, y_known) -> float:
         f = interp1d(x_known, y_known, kind="linear")
         return f(x)
 
-class H2O(Material):
-    def __init__(self, phase="liquid") -> None:
-        M = 18.015 # kg/kmol
-        R = 0.4615 # kJ/kg-K
-        super().__init__(M=M, R=R)
-        self._phase = phase
-
-        property_table = "H2O-sat-temperature.csv"
-        df = pd.read_csv(PROPERTY_TABLE_PATH+property_table)
-
-        self._T = df["T"].to_numpy()
-        self._Psat = df["Psat"].to_numpy()
-        self._ufg = df["ufg"].to_numpy()
-        self._hfg = df["hfg"].to_numpy()
-        self._sfg = df["sfg"].to_numpy()
-        if self._phase == "liquid":
-            self._v = df["vf"].to_numpy()
-            self._h = df["hf"].to_numpy()
-            self._u = df["uf"].to_numpy()
-            self._s = df["sf"].to_numpy()
-        elif self._phase in ["vapor", "gas"]:
-            self._v = df["vg"].to_numpy()
-            self._h = df["hg"].to_numpy()
-            self._u = df["ug"].to_numpy()
-            self._s = df["sg"].to_numpy()
-
-    def enthalpy(self, T):
-        """Specific enthalpy [kJ/kg]"""
-        return self._interpolate(T, self._T, self._h)
-
 class IdealGas(Material):
-    def __init__(self, M: float, R: float, property_file: str, Cp_coefs: list) -> None:
-        super().__init__(M=M, R=R)
+    def __init__(self, M: float, Cp_coefs: list) -> None:
+        super().__init__(M=M)
         self._Cp_coefs = Cp_coefs
-
-        df = pd.read_csv(PROPERTY_TABLE_PATH+property_file)
-        self._T = df["T"].to_numpy()
-        self._h_bar = df["h-bar"].to_numpy()
-        self._u_bar = df["u-bar"].to_numpy()
-        self._s_bar = df["s-bar"].to_numpy()
 
     def specific_heat(self, T) -> float:
         """
@@ -64,48 +27,50 @@ class IdealGas(Material):
         Args:
             - T (float): temperature [K]
         """
-        a, b, c, d = self._Cp_coefs
-        return a + b*T + c*T*T + d*T*T*T
-
-class O2(IdealGas):
-    def __init__(self) -> None:
-        M = 31.999 # kg/kmol
-        R = 0.2598 # kJ/kg-K
-        property_table = "O2-ideal-gas.csv"
-        Cp_coefs = [25.48, 1.520e-2, -0.7155e-5, 1.312e-9]
-        super().__init__(M, R, property_table, Cp_coefs)
+        a, b, c, d, e, f, g = self._Cp_coefs
+        return a + b*T + c*T**2 + d*T**3 + e*T**4 + f*T**5 + g*T**6
 
 class N2(IdealGas):
     def __init__(self) -> None:
         M = 28.013 # kg/kmol
-        R = 0.2968 # kJ/kg-K
-        property_table = "N2-ideal-gas.csv"
-        Cp_coefs = [28.90, -0.1571e-2, 0.8081e-5, -2.873e-9]
-        super().__init__(M, R, property_table, Cp_coefs)
+        Cp_coefs = [3.675,	-1.21E-03,	2.32E-06,	-6.32E-10,	-2.26E-13,	0.00E+00,	0.00E+00]
+        super().__init__(M, Cp_coefs)
+
+class O2(IdealGas):
+    def __init__(self) -> None:
+        M = 31.999 # kg/kmol
+        Cp_coefs = [3.626,	-1.88E-03,	7.06E-06,	-6.76E-09,	2.16E-12,	0.00E+00,	0.00E+00]
+        super().__init__(M, Cp_coefs)
+
+class H2O(IdealGas):
+    def __init__(self) -> None:
+        M = 18.0155 # kg/kmol
+        Cp_coefs = [4.07,	-1.11E-03,	4.15E-06,	-2.96E-09,	8.07E-13,	0.00E+00,	0.00E+00]
+        super().__init__(M, Cp_coefs)
+
+class H2(IdealGas):
+    def __init__(self) -> None:
+        M = 2.016 # kg/kmol
+        Cp_coefs = [3.057,	2.68E-03,	-5.81E-06,	5.52E-09,	-1.81E-12,	0.00E+00,	0.00E+00]
+        super().__init__(M, Cp_coefs)
 
 class CO2(IdealGas):
     def __init__(self) -> None:
         M = 44.01  # kg/kmol
-        R = 0.1889 # kJ/kg-K
-        property_table = "CO2-ideal-gas.csv"
-        Cp_coefs = [22.26, 5.981e-2, -3.501e-5, 7.469e-9]
-        super().__init__(M, R, property_table, Cp_coefs)
+        Cp_coefs = [2.401,	8.74E-03,	-6.61E-06,	2.00E-09,	0.00E+00,	0.00E+00,	0.00E+00]
+        super().__init__(M, Cp_coefs)
 
 class CO(IdealGas):
     def __init__(self) -> None:
-        M = 28.011 # kg/kmol
-        R = 0.2968 # kJ/kg-K
-        property_table = "CO-vapor-ideal-gas.csv"
-        Cp_coefs = [28.16, 0.1675e-2, 0.5375e-5, -2.222e-9]
-        super().__init__(M, R, property_table, Cp_coefs)
+        M = 28.0105 # kg/kmol
+        Cp_coefs = [3.71,	-1.62E-03,	3.69E-06,	-2.03E-09,	2.40E-13,	0.00E+00,	0.00E+00]
+        super().__init__(M, Cp_coefs)
 
-class H2O_vapor_IG(IdealGas):
+class Ar(IdealGas):
     def __init__(self) -> None:
-        M = 18.015 # kg/kmol
-        R = 0.4615 # kJ/kg-K
-        property_table = "H2O-vapor-ideal-gas.csv"
-        Cp_coefs = [32.24, 0.1923e-2, 1.055e-5, -3.595e-9]
-        super().__init__(M, R, property_table, Cp_coefs)
+        M = 39.948 # kg/kmol
+        Cp_coefs = [2.5,	0.00E+00,	0.00E+00,	0.00E+00,	0.00E+00,	0.00E+00,	0.00E+00]
+        super().__init__(M, Cp_coefs)
 
 if __name__ == "__main__":
     print(H2O(phase="vapor").enthalpy(25))
---
+++ IGmodel/mixture.py
@@ -0,0 +1,35 @@
+import numpy as np
+import IGmodel.materials as mat
+
+class Mixture:
+    def __init__(self, T, P, N2=0.0, O2=0.0, H2O=0.0, H2=0.0, CO2=0.0, CO=0.0, Ar=0.0) -> None:
+        self.T = T
+        self.P = P
+        self._yi = np.array([N2, O2, H2O, H2, CO2, CO, Ar])
+        self._materials = np.array([mat.N2(), mat.O2(), mat.H2O(), mat.H2(), mat.CO2(), mat.CO(), mat.Ar()])
+
+        Mi = []
+        for comp in self._materials:
+            Mi.append(comp.M)
+
+        self.M = np.dot(self._yi, Mi)
+
+        assert self._yi.sum() == 1.0
+
+    def __repr__(self):
+        print_str = "Mixture Composition\n"
+        print_str += f"N2:  {self._yi[0]}\n"
+        print_str += f"O2:  {self._yi[1]}\n"
+        print_str += f"H2O: {self._yi[2]}\n"
+        print_str += f"H2:  {self._yi[3]}\n"
+        print_str += f"CO2: {self._yi[4]}\n"
+        print_str += f"CO:  {self._yi[5]}\n"
+        print_str += f"Ar:  {self._yi[6]}\n"
+        print_str += "\n"
+        print_str += f"Molar Mass: {self.M:.3f} kg/kmol\n"
+
+        return print_str
+
+if __name__ == "__main__":
+    mixture = Mixture(T=1300, P=3000, N2=0.79, O2=0.21)
+    print(mixture)
